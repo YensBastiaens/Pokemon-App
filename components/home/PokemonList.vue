1cd09f54d93d68da77266eb8687eac10c86b8817
yens <yens.b@hotmail.be>
2022-06-23 02:59:57 +0200

    API Pokemon list in for loop - name, number, types of pokemon displayed (images not yet) - box shadow added to every pokemons section

--- components/home/PokemonList.vue
+++ components/home/PokemonList.vue
@@ -2,6 +2,7 @@
 // import { onMounted } from 'vue';
 // import axios from 'axios'
 import { useAxios } from '@vueblocks/vue-use-axios'
+
 export default {
     name: 'Home',
     setup() {
@@ -9,7 +10,6 @@ export default {
         const url = 'https://stoplight.io/mocks/appwise-be/pokemon/57519009/pokemon'
         const { data } = useAxios(url)
         const pokemons = ref(data)
-
         console.log(pokemons)
         return { pokemons }
     }
@@ -22,7 +22,24 @@ export default {
 <template>
     <section>
         <div class="container">
+            <ul v-for="(pokemon) in pokemons">
+                <div class="individualPokemon my-[1rem] flex gap-[1rem] ">
+                    <img src="" alt='pokemon'>
+                    <!-- <div>{{pokemon.sprites.front_default}}</div> -->
+                    <div class="flex flex-col">
+                        <li class="text-[1.7rem] leading-[2rem] font-bold capitalize tracking-[0.0374rem]">{{
+                                pokemon.name
+                        }}</li>
+                        <li class="text-[#919698] font-[400] text-[1.5rem] leading-[1.8rem]">Nr. {{ pokemon.id }}</li>
+                    </div>
 
+                    <div v-for="type in pokemon.types"
+                        class="bg-red-500 rounded-[10rem] p-[3px_10px] h-[2rem] flex items-center justify-center text-[1.2rem] font-[400] leading-[1.4rem] tracking-[0.0374rem;]">
+                        {{ type.type.name }}
+                    </div>
+                </div>
+
+            </ul>
         </div>
     </section>
 </template>
@@ -30,4 +47,9 @@ export default {
 
 
 <style scoped>
+.individualPokemon {
+    background: #FFFFFF;
+    box-shadow: 0px 15px 15px rgba(0, 0, 0, 0.04);
+    border-radius: 10px;
+}
 </style>
